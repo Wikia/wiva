c1a083af4d6b8b76a96d6d2474a19590f50d46f6
Aleksander Balicki <alistra@wikia-inc.com>
2015-07-23 17:01:21 +0200

    Parser updates

--- wikitext/parser.py
+++ wikitext/parser.py
@@ -36,8 +36,13 @@ def p_object_number(p):
     p[0] = p[1]
 
 
-def p_object_comma(p):
-    'object : COMMA'
+def p_object_misc(p):
+    """object : COMMA
+              | DOT
+              | TIMES
+              | htmlcomment
+              | SQUOTE
+              | COLON"""
     p[0] = p[1]
 
 
@@ -52,7 +57,7 @@ def p_object_curly2_name(p):
 
 
 def p_object_curly2_name_args(p):
-    'object : LCURLY LCURLY NAME PIPE pipelist RCURLY RCURLY'
+    'object : LCURLY LCURLY NAME PIPE pipelisttemplate RCURLY RCURLY'
     p[0] = {'type': "template", 'value': p[2]}
 
 
@@ -62,21 +67,68 @@ def p_object_paren_name_args(p):
 
 
 def p_object_squote3_name(p):
-    'object : SQUOTE SQUOTE SQUOTE NAME SQUOTE SQUOTE SQUOTE'
+    'object : SQUOTE SQUOTE SQUOTE names SQUOTE SQUOTE SQUOTE'
     pass
 
 
+def p_object_squote2_name(p):
+    'object : SQUOTE SQUOTE names SQUOTE SQUOTE'
+    pass
+
+
+def p_object_dquote_name(p):
+    'object : DQUOTE names DQUOTE'
+    pass
+
+
+def p_object_html_tag_closed(p):
+    'object : LANGLE NAME attributes DIVIDE RANGLE'
+    pass
+
+
+def p_object_html_tag(p):
+    'object : LANGLE NAME attributes RANGLE article LANGLE DIVIDE NAME RANGLE'
+    pass
+
+
+def p_attributes_recursive(p):
+    'attributes : attributes NAME EQUALS DQUOTE NAME DQUOTE'
+    pass
+
+
+def p_attributes_base(p):
+    'attributes : empty'
+    pass
+
+def p_htmlcomment(p):
+    'htmlcomment : LANGLE BANG DASH DASH names DASH DASH RANGLE'
+
 def p_insidesquare2_name(p):
-    'insidesquare2 : NAME'
-
-
-def p_insidesquare2_name_with_colon(p):
-    'insidesquare2 : NAME COLON NAME'
+    'insidesquare2 : names'
+    pass
 
 
 def p_insidesquare2_name_with_pipe(p):
-    'insidesquare2 : NAME COLON NAME PIPE pipelist'
+    'insidesquare2 : names PIPE pipelist'
+
+
+def p_insidesquare2_name_with_colon(p):
+    'insidesquare2 : names COLON names'
+
+
+def p_insidesquare2_name_with_colon_and_pipe(p):
+    'insidesquare2 : names COLON names PIPE pipelist'
     pass
+
+
+def p_pipelisttemplate_recursive(p):
+    'pipelisttemplate : pipelisttemplate PIPE article EQUALS article'
+    p[0] = p[1] + [p[3]]
+
+
+def p_pipelisttemplate_base(p):
+    'pipelisttemplate : article'
+    p[0] = [p[1]]
 
 
 def p_pipelist_recursive(p):
@@ -87,6 +139,19 @@ def p_pipelist_recursive(p):
 def p_pipelist_base(p):
     'pipelist : article'
     p[0] = [p[1]]
+
+
+def p_names_base(p):
+    'names : NAME'
+    pass
+
+
+def p_names_recursive(p):
+    """names : names NAME
+             | names DASH
+             | names COLON
+    """
+    pass
 
 
 # Error rule for syntax errors
