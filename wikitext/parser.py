8ec378d4f6e469fa425e2d3273dd45cec363c05c
Aleksander Balicki <alistra@wikia-inc.com>
2015-07-23 17:27:20 +0200

    further attempt at parsing

--- wikitext/parser.py
+++ wikitext/parser.py
@@ -41,9 +41,21 @@ def p_object_misc(p):
               | DOT
               | TIMES
               | htmlcomment
+              | header
               | SQUOTE
-              | COLON"""
+              | COLON
+              | DASH"""
     p[0] = p[1]
+
+
+def p_header(p):
+    """header : EQUALS names EQUALS
+              | EQUALS EQUALS names EQUALS EQUALS
+              | EQUALS EQUALS EQUALS names EQUALS EQUALS EQUALS
+              | EQUALS EQUALS EQUALS EQUALS names EQUALS EQUALS EQUALS EQUALS
+              | EQUALS EQUALS EQUALS EQUALS EQUALS names EQUALS EQUALS EQUALS EQUALS EQUALS
+              | EQUALS EQUALS EQUALS EQUALS EQUALS EQUALS names EQUALS EQUALS EQUALS EQUALS EQUALS EQUALS
+    """
 
 
 def p_object_square2(p):
@@ -52,12 +64,12 @@ def p_object_square2(p):
 
 
 def p_object_curly2_name(p):
-    'object :  LCURLY LCURLY NAME RCURLY RCURLY'
+    'object :  LCURLY LCURLY names RCURLY RCURLY'
     p[0] = {'type': "template", 'value': p[2]}
 
 
 def p_object_curly2_name_args(p):
-    'object : LCURLY LCURLY NAME PIPE pipelisttemplate RCURLY RCURLY'
+    'object : LCURLY LCURLY names PIPE pipelisttemplate RCURLY RCURLY'
     p[0] = {'type': "template", 'value': p[2]}
 
 
@@ -73,6 +85,11 @@ def p_object_squote3_name(p):
 
 def p_object_squote2_name(p):
     'object : SQUOTE SQUOTE names SQUOTE SQUOTE'
+    pass
+
+
+def p_object_squote_name(p):
+    'object : SQUOTE names SQUOTE'
     pass
 
 
@@ -100,8 +117,10 @@ def p_attributes_base(p):
     'attributes : empty'
     pass
 
+
 def p_htmlcomment(p):
     'htmlcomment : LANGLE BANG DASH DASH names DASH DASH RANGLE'
+
 
 def p_insidesquare2_name(p):
     'insidesquare2 : names'
@@ -122,12 +141,12 @@ def p_insidesquare2_name_with_colon_and_pipe(p):
 
 
 def p_pipelisttemplate_recursive(p):
-    'pipelisttemplate : pipelisttemplate PIPE article EQUALS article'
+    'pipelisttemplate : pipelisttemplate PIPE names EQUALS article'
     p[0] = p[1] + [p[3]]
 
 
 def p_pipelisttemplate_base(p):
-    'pipelisttemplate : article'
+    'pipelisttemplate : names'
     p[0] = [p[1]]
 
 
@@ -150,6 +169,8 @@ def p_names_recursive(p):
     """names : names NAME
              | names DASH
              | names COLON
+             | names SQUOTE
+             | LPAREN names RPAREN
     """
     pass
 
@@ -161,14 +182,14 @@ def p_error(p):
     prev_line_pos = lexer.lexdata.find('\n', max(pos - MAX_CONTEXT, 0), pos)
     next_line_pos = lexer.lexdata.find('\n', pos)
     if prev_line_pos == -1:
-        prev_line_pos = 0
+        prev_line_pos = max(pos - MAX_CONTEXT, 0)
 
-    if next_line_pos > pos + MAX_CONTEXT:
+    if next_line_pos > pos + MAX_CONTEXT or next_line_pos == -1:
         next_line_pos = pos + MAX_CONTEXT
 
     print colored("Syntax error in input!", "red")
     print
-    print colored(lexer.lexdata[prev_line_pos:next_line_pos], "yellow")
+    print colored(lexer.lexdata[prev_line_pos:next_line_pos], "yellow").encode('utf-8')
     print
 
 
