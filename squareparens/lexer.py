dc4c5dd63f56f3b29cf139d34a6de81add89ecce
Aleksander Balicki <alistra@wikia-inc.com>
2015-07-24 10:56:57 +0200

    Only parens parser

---
+++ squareparens/lexer.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+__author__ = 'alistra'
+
+# coding=utf-8
+import re
+
+tokens = (
+    'TEXT', 'LPAREN', 'RPAREN'
+)
+
+
+def t_TEXT(t):
+    r'[^\[\]]+'
+    return t
+
+
+def t_LPAREN(t):
+    r'\['
+    return t
+
+
+def t_RPAREN(t):
+    r'\]'
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+import ply.lex as lex
+
+lexer = lex.lex(reflags=re.UNICODE)
+
+
+def tokenize(text):
+    lexer.input(text)
+    return list(lexer)
---
+++ squareparens/parser.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+__author__ = 'alistra'
+
+# coding=utf-8
+import logging
+from termcolor import colored
+import ply.yacc as yacc
+# noinspection PyUnresolvedReferences
+from squareparens.lexer import *
+
+
+def p_empty(p):
+    'empty :'
+    return None
+
+
+def p_article_base(p):
+    'article : empty'
+    p[0] = []
+
+
+def p_article_recursive(p):
+    'article : article TEXT'
+    p[0] = p[1] + [p[2]]
+
+
+def p_article_recursive_parens(p):
+    'article : article LPAREN article RPAREN'
+    p[0] = p[1] + [{"type": "in-squareparens", "value": p[3]}]
+
+
+# Error rule for syntax errors
+def p_error(p):
+    MAX_CONTEXT = 50
+    pos = p.lexpos
+    prev_line_pos = lexer.lexdata.find('\n', max(pos - MAX_CONTEXT, 0), pos)
+    next_line_pos = lexer.lexdata.find('\n', pos)
+    if prev_line_pos == -1:
+        prev_line_pos = max(pos - MAX_CONTEXT, 0)
+
+    if next_line_pos > pos + MAX_CONTEXT or next_line_pos == -1:
+        next_line_pos = pos + MAX_CONTEXT
+
+    print colored("Syntax error in input!", "red")
+    print
+    print colored(lexer.lexdata[prev_line_pos:next_line_pos], "yellow").encode('utf-8')
+    print
+
+
+# Build the parser
+parser = yacc.yacc(start='article')
+
+
+def parse(text):
+    return parser.parse(text, debug=logging.getLogger())
